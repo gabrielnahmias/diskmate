e99bcd0268495aae69d00a156baea33c045e7eb0
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2012-07-07 22:26:33 -0500

    My delicious cookie functions.

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,35 @@
-// JavaScript Document
+function deleteCookie(sCookie) {
+	
+	setCookie(sCookie, "", 0);
+	
+}
+
+function getCookie(sName) {
+	
+	var i, sCName, y, arrCookies = document.cookie.split(";");
+	
+	for (i = 0; i < arrCookies.length; i++) {
+		
+		sCName = arrCookies[i].substr(0, arrCookies[i].indexOf("=") );
+		sValue = arrCookies[i].substr( arrCookies[i].indexOf("=") + 1 );
+		
+		sCName = sCName.replace(/^\s+|\s+$/g, "");
+		
+		if (sCName == sName)
+			return unescape(sValue);
+			
+	}
+	
+}
+
+function setCookie(sName, value, intDays) {
+	
+	var objDate = new Date();
+	
+	objDate.setDate( objDate.getDate() + intDays );
+	
+	var sValue = escape(value) + ( (intDays == null) ? "" : "; expires=" + objDate.toUTCString() );
+	
+	document.cookie = sName + "=" + sValue;
+	
+}
